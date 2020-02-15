025439d8f54f57435a77cde1c3a5f852383eb8a7
graceferguson <grace.c.ferguson@hotmail.com>
2020-02-14 19:53:29 -0700

    Create MovementControl.cs

---
+++ MovementControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControl : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 0.5f;
+
+    private Transform thisTransform;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalAxis = Input.GetAxis("Horizontal");
+        var verticalAxis = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(
+            horizontalAxis * Speed * Time.deltaTime,
+            0.0f,
+            verticalAxis * Speed * Time.deltaTime);
+
+        thisTransform.Translate(move);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+
+}
